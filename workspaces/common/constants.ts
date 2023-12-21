ffc5ae38f53bc8932610d7143a2654682bc89adf
Nicholas Kolba <nick@connectifi.co>
2023-12-20 22:49:45 -0500

    added basic exate hook

--- workspaces/common/constants.ts
+++ workspaces/common/constants.ts
@@ -5,3 +5,9 @@ export const OPENFIGI_TICKER_INFO_URL = `${OPENFIGI_HOST}/v3/search`;
 export const POLYGON_HOST = 'https://api.polygon.io';
 
 export const POLYGON_TICKER_INFO_URL = `${POLYGON_HOST}/vX/reference/tickers`;
+
+export const EXATE_HOST = "https://api.exate.co";
+
+export const EXATE_ID_URL = `${EXATE_HOST}/apigator/identity/v1/token`;
+
+export const EXATE_DATA_URL = `${EXATE_HOST}/apigator/protect/v1/dataset`;
