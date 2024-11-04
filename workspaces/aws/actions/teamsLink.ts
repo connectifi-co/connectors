c4739bf935a18bf9b4afa3828fab8cca0bb256c5
Nicholas Kolba <nick@connectifi.co>
2024-11-04 12:32:20 -0500

    example teams connector for deep links (#14)
    * example teams connector for deep links
    
    * adding missed change
    
    ---------
    
    Co-authored-by: Brian Schwinn <bschwinn@users.noreply.github.com>

---
+++ workspaces/aws/actions/teamsLink.ts
@@ -0,0 +1,8 @@
+import type { ActionRequest } from "../../common/lib/types"
+import { teamsLink } from "../../common/actions/links/teamsLink";
+
+export async function handler(event:any) {
+  const { context, intent } = JSON.parse(event.body) as ActionRequest;
+
+  return teamsLink({context, intent});
+}
