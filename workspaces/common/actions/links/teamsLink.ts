dd809400cad229415ec7ff492649abe34e253e5c
Nicholas Kolba <nick@connectifi.co>
2024-11-04 12:32:20 -0500

    example teams connector for deep links (#14)
    * example teams connector for deep links
    
    * adding missed change
    
    ---------
    
    Co-authored-by: Brian Schwinn <bschwinn@users.noreply.github.com>

---
+++ workspaces/common/actions/links/teamsLink.ts
@@ -0,0 +1,68 @@
+import { createResponse } from '../../lib/utils';
+import { ActionHandler, HTTPResponse } from '../../lib/types';
+import { Context, Contact, ContactList } from '@finos/fdc3';
+
+
+interface ChatInitSettings {
+  type: 'fdc3.chat.intiSettings';
+  message: Message;
+  members: ContactList;
+}
+
+interface Message {
+  text: {[key:string]: string};
+}
+
+const getEmailList = (contactList: ContactList): string => {
+  if (contactList){
+    const emailArray = contactList.contacts.map((contact) => {
+      return contact.id?.email;
+    });
+    return emailArray.join(',');
+  }
+  return '';
+};
+
+const handleInitSettings = (context: Context) : HTTPResponse => {
+  const settings = context as ChatInitSettings;
+  let emails = '';
+  if (settings.members){
+    emails = getEmailList(settings.members as ContactList);
+  }
+  const message = settings.message as Message;
+  let body = '';
+  if (message?.text['text/html']){
+    body = encodeURIComponent(message.text['text/html']);
+  }
+  else if (message?.text['text\/plain']){
+    body = message.text['text/plain'];
+  }
+  console.log('handleInitSettings!', body);
+  const url = `https://teams.microsoft.com/l/chat/0/0?users=${emails}&message=${body}`;
+  return createResponse(200, {url});
+};
+
+const handleContactList = (context: ContactList) : HTTPResponse => {
+  const  emails = getEmailList(context);
+  const url = `https://teams.microsoft.com/l/chat/0/0?users=${emails}`;
+  return createResponse(200, {url});
+}
+
+export const teamsLink: ActionHandler = async (params) => {
+  const { context, intent } = {...params};
+    if (context.type === "fdc3.contact"){
+      const url = `https://teams.microsoft.com/l/chat/0/0?users=${(context as Contact).id.email}`;
+      return createResponse(200, {url});
+    }
+    if (context.type === "fdc3.chat.initSettings"){
+      return handleInitSettings(context);
+    }
+    if (context.type === "fdc3.contactList"){
+      const contactList = context as ContactList;
+      return handleContactList(contactList);
+    }
+  
+  return createResponse(400, {
+    message: 'unhandled intent and/or context type',
+  });
+};
