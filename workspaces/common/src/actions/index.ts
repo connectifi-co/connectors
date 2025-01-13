17bc29eca68ef4505d1716f70606a2c98db47d23
bschwinn <bschwinn7@gmail.com>
2025-01-13 14:18:25 -0500

    rename folder to match interfaces and action type names

--- workspaces/common/src/actions/index.ts
+++ workspaces/common/src/actions/index.ts
@@ -5,8 +5,8 @@ import { mapLink } from './links/maps';
 import { companyHQLink } from './links/polygon';
 import { emailLink } from './links/email';
 import { slackLink } from './links/slack';
-import { polygonIOHandler } from './api/polygon';
-import { openAIHandler } from './api/openAI';
+import { polygonIOHandler } from './data/polygon';
+import { openAIHandler } from './data/openAI';
 
 const ActionsMap = () => {
   const actions: Map<string, LinkActionHandler | DataActionHandler> = new Map();
