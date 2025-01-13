eed6212a93418008dbefd167436a695858200239
bschwinn <bschwinn7@gmail.com>
2025-01-13 14:10:03 -0500

    consolidate stuff

--- workspaces/common/src/actions/links/slack.ts
+++ workspaces/common/src/actions/links/slack.ts
@@ -1,7 +1,7 @@
 import type { LinkActionHandler } from '@connectifi/sdk';
 import { ContextTypes } from '@finos/fdc3';
 import { RequestError, ServerError } from '../../types';
-import { getSlackIDs } from '../../slack';
+import { SLACK_USER_LOOKUP_URL } from '../../constants';
 
 export const slackLink: LinkActionHandler = async (request) => {
   const apiKey = process.env.SLACK_API_KEY;
@@ -17,4 +17,27 @@ export const slackLink: LinkActionHandler = async (request) => {
   const ids = await getSlackIDs(apiKey, context.id.email);
   const url = `slack://user?team=${ids.teamId}&id=${ids.userId}`;
   return { url };
+};
+
+interface SlackIDs {
+  teamId: string;
+  userId: string;
+}
+
+const getSlackIDs = async (
+  apiKey: string,
+  email: string,
+): Promise<SlackIDs> => {
+  const apiURL = SLACK_USER_LOOKUP_URL + email;
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${apiKey}`,
+  };
+
+  const res = await fetch(apiURL, { method: 'GET', headers });
+  const resJson: any = await res.json();
+  return {
+    teamId: resJson.user.team_id,
+    userId: resJson.user.id,
+  };
 };
