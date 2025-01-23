dce34badbaceb3f3f7bdbe3c94143d374ea9d11f
Nicholas Kolba <nick@connectifi.co>
2025-01-23 11:32:06 -0500

    ported new handlers over from old structure

--- workspaces/common/src/actions/data/openAI/generate.ts
+++ workspaces/common/src/actions/data/openAI/generate.ts
@@ -2,49 +2,76 @@ import type { Context } from '@finos/fdc3';
 import { Prompt, Completion } from '../../../types';
 import OpenAI from 'openai';
 
-export const generate = async (
-  apiKey: string,
-  prompt: Prompt,
-): Promise<Completion> => {
-  const openai = new OpenAI({ apiKey: apiKey });
+/*
+example prompts:
+    {
+        "type": "fdc3.prompt",
+        "text": "What can you tell me about this company?",
+        "context": {
+            "type": "fdc3.instrument",
+            "id": {
+                "ticker": "IBM"
+            }
+        }
+    }
 
-  const messages: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [];
-  if (prompt.context) {
-    messages.push({
-      role: 'system',
-      content: createSystemPrompt(prompt.context),
-    });
-    messages.push({ role: 'user', content: contextToPrompt(prompt.context) });
+    {
+    "type": "fdc3.prompt",
+    "text": "Write a cold email to send to this contact",
+    "context": {
+        "type": "fdc3.contact",
+        "name": "John Smith",
+        "id": {
+            "email": "john.smith@example.com"
+        }
+    }
   }
-  messages.push({ role: 'user', content: prompt.text });
+*/
+const contextDescriptors = [
+  `Context type of 'fdc3.instrument' describes a stock or other financial instrument.  The 'id' property lists common identifiers for the instrument such as 'ticker'.`,
+  `Context type of 'fdc3.contact' describes a person or contact - typically from a CRM or similar system.  The 'id' property lists common identifiers for the contact such as 'email'.`,
+];
 
-  const chatCompletion = await openai.chat.completions.create({
-    messages,
-    model: 'gpt-4o-mini',
-  });
+const createSystemPrompt = ( context: Context) => { return `
+  You will be provided with a text prompt and a FDC3 context data object of type '${context.type}' as background for this prompt.  
+  Use the FDC3 context data object as background for the text you will generate in response to the prompt.  
+  Here are some kinds of things that a context data object could describe, based on the context type:
+  ${contextDescriptors.join('/n')}
+`};
 
-  if (chatCompletion?.choices.length > 0) {
-    return {
-      type: 'connect.completion',
-      text: chatCompletion.choices[0].message.content || '',
-    };
-  }
-
-  return {
-    type: 'connect.completion',
-    text: 'Error: Please Try Again',
-  };
+const contextToPrompt = (context: Context): string => {
+  return `Use the following FDC3 context data object of type '${context.type}' as background:
+  ${JSON.stringify(context)}
+  `;
 };
 
-const createSystemPrompt = (context: Context) => `
-    You will be provided with a text prompt and a FDC3 context data object of type '${context.type}' as background for this prompt.  
-    Use the FDC3 context data object as background for the text you will generate in response to the prompt.  
-    Here are some kinds of things that a context data object could describe, based on the context type:
-    Context type of 'fdc3.instrument' describes a stock or other financial instrument.  The 'id' property lists common identifiers for the instrument such as 'ticker'.
-    Context type of 'fdc3.contact' describes a person or contact - typically from a CRM or similar system.  The 'id' property lists common identifiers for the contact such as 'email'.
-`;
+export const generate = async (apiKey: string, prompt: Prompt):Promise<Completion> => {
+ 
+  const openai = new OpenAI({
+      apiKey: apiKey,
+  });
 
-const contextToPrompt = (context: Context): string => `
-    Use the following FDC3 context data object of type '${context.type}' as background:
-    ${JSON.stringify(context)}
-`;
+  const messages: Array<OpenAI.Chat.Completions.ChatCompletionMessageParam> = [];
+  if (prompt.context){
+      messages.push( { role: "system", content: createSystemPrompt(prompt.context)});
+      messages.push({ role: "user", content: contextToPrompt(prompt.context)});
+  }
+  messages.push({ role: "user", content: prompt.text});
+
+  const chatCompletion = await openai.chat.completions.create({
+      messages,
+      model: "gpt-4o-mini",
+  });
+
+  if (chatCompletion?.choices.length > 0){
+     return  {
+      type:'connect.completion',
+      text: chatCompletion.choices[0].message.content || ''
+     };
+  }
+  
+  return {
+      type:'connect.completion',
+      text: 'Error: Please Try Again'
+  }
+}
--- workspaces/common/src/actions/data/openAI/index.ts
+++ workspaces/common/src/actions/data/openAI/index.ts
@@ -7,6 +7,8 @@ import {
 import { generate } from './generate';
 import { summarize } from './summarize';
 import { findSimilar } from './findSimilar';
+import { generatePrompt } from './generatePrompt';
+import { getEntities } from './getEntities';
 
 const apiKey = process.env.OPEN_AI_API_KEY;
 
@@ -14,24 +16,29 @@ export const openAIHandler: DataActionHandler = async (params) => {
   if (!apiKey) {
     throw new ServerError('openAI api key missing');
   }
-
+  
   const { context, intent } = { ...params };
-  if (
-    intent !== 'Generate' &&
-    intent !== 'FindSimilar' &&
-    intent !== 'Summarize'
-  ) {
-    throw new RequestError('intent not supported');
-  }
 
-  if (intent === 'Generate') {
+  if (intent === "Generate") {
     const completion = await generate(apiKey, context as Prompt);
     return completion;
-  } else if (intent === 'Summarize') {
-    const summary = await summarize(apiKey, context);
-    return summary;
-  } else if (intent === 'FindSimilar') {
+  }
+  if (intent === "GeneratePrompt") {
+    const prompt = await generatePrompt(apiKey, context);
+    return prompt;
+  }
+  if (intent === "Summarize") {
+      const summary = await summarize(apiKey, context);
+      return summary;
+  }
+  if (intent === "FindSimilar"){
     const list = await findSimilar(apiKey, context);
     return list;
   }
+  if (intent === "GetEntities"){
+    const entities = await getEntities(apiKey, context);
+    return entities;
+  }
+
+  throw new RequestError('intent not supported');
 };
